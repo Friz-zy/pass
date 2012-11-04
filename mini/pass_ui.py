9735ba71c3f6e0402a5cf005574e0d13bca2f1da
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2012-11-04 13:29:53 +0300

    first commit

---
+++ mini/pass_ui.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pass.ui'
+#
+# Created: Sat Oct 13 15:48:59 2012
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Pass(object):
+    def setupUi(self, Pass):
+        Pass.setObjectName(_fromUtf8("Pass"))
+        Pass.resize(361, 313)
+        self.gridLayout = QtGui.QGridLayout(Pass)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.labelUser = QtGui.QLabel(Pass)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.labelUser.sizePolicy().hasHeightForWidth())
+        self.labelUser.setSizePolicy(sizePolicy)
+        self.labelUser.setObjectName(_fromUtf8("labelUser"))
+        self.verticalLayout.addWidget(self.labelUser)
+        self.lineEditUser = QtGui.QLineEdit(Pass)
+        self.lineEditUser.setObjectName(_fromUtf8("lineEditUser"))
+        self.verticalLayout.addWidget(self.lineEditUser)
+        self.labelURL = QtGui.QLabel(Pass)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.labelURL.sizePolicy().hasHeightForWidth())
+        self.labelURL.setSizePolicy(sizePolicy)
+        self.labelURL.setObjectName(_fromUtf8("labelURL"))
+        self.verticalLayout.addWidget(self.labelURL)
+        self.lineEditURL = QtGui.QLineEdit(Pass)
+        self.lineEditURL.setObjectName(_fromUtf8("lineEditURL"))
+        self.verticalLayout.addWidget(self.lineEditURL)
+        self.labelPass = QtGui.QLabel(Pass)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.labelPass.sizePolicy().hasHeightForWidth())
+        self.labelPass.setSizePolicy(sizePolicy)
+        self.labelPass.setObjectName(_fromUtf8("labelPass"))
+        self.verticalLayout.addWidget(self.labelPass)
+        self.lineEditPass = QtGui.QLineEdit(Pass)
+        self.lineEditPass.setObjectName(_fromUtf8("lineEditPass"))
+        self.verticalLayout.addWidget(self.lineEditPass)
+        self.pushButton = QtGui.QPushButton(Pass)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout.addWidget(self.pushButton)
+        spacerItem = QtGui.QSpacerItem(20, 18, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.lineEditGive = QtGui.QLineEdit(Pass)
+        self.lineEditGive.setObjectName(_fromUtf8("lineEditGive"))
+        self.verticalLayout.addWidget(self.lineEditGive)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+        self.retranslateUi(Pass)
+        QtCore.QMetaObject.connectSlotsByName(Pass)
+
+    def retranslateUi(self, Pass):
+        Pass.setWindowTitle(QtGui.QApplication.translate("Pass", "Pass", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelUser.setText(QtGui.QApplication.translate("Pass", "User:", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelURL.setText(QtGui.QApplication.translate("Pass", "Url:", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelPass.setText(QtGui.QApplication.translate("Pass", "Pass:", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("Pass", "Give It!", None, QtGui.QApplication.UnicodeUTF8))
+
---
+++ mini/window.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# coding=utf-8
+# http://habrahabr.ru/post/31426/
+from pass_ui import Ui_Pass
+from PyQt4 import QtCore, QtGui
+import sys
+
+
+class Pass(QtGui.QWidget):
+	def __init__(self, parent=None):
+		QtGui.QWidget.__init__(self, parent)
+		self.ui = Ui_Pass()
+		self.ui.setupUi(self)
+		self.ui.lineEditPass.setEchoMode(self.ui.lineEditPass.Password)
+		QtCore.QObject.connect(self.ui.pushButton,QtCore.SIGNAL("clicked()"), self.add)
+
+	def add(self):
+		import hashlib
+		# use pass.py as salt and check the unique
+		with open('pass.py') as f:
+			salt = f.readlines()
+		# if salt isn't unique: add some unique string to it
+		if "#" in salt[-1][1]:
+			import os.path
+			try:
+				os.popen("(dmesg;env;head -c16 /dev/random)|sha512sum >> ./pass.py")
+				with open('pass.py', "r") as f:
+					salt = f.readlines()
+				salt[-1]="#" + salt[-1]
+				with open('pass.py', "w") as f:
+					f.writelines(salt)
+				os.popen("chmod 400 ./pass.py") # only your user can read this file
+			except IOError, OSError:
+				# it is works in all OS, but clock() is most efficient in Windows
+				from time import clock
+				with open('pass.py', "a") as f:
+					f.writeline("#" + hashlib.sha512(clock()).hexdigest())
+
+		from itertools import cycle, izip
+		# XOR function, thk The Internet
+		def xor(ss, key):
+			key = cycle(key)
+			return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(ss, key))
+
+		with open('pass.py') as f:
+			salt = f.read()
+		nick = str(self.ui.lineEditUser.text()) + " : " + str(self.ui.lineEditURL.text())
+		firstXor = process(salt, nick)
+		self.ui.lineEditURL.clear()
+		self.ui.lineEditUser.clear()
+		firstSha = hashlib.sha512(firstXor).hexdigest()
+		password = hashlib.sha512(process(firstSha, str(self.ui.lineEditPass.text()))).hexdigest()[:32]
+		self.ui.lineEditPass.clear()
+		self.ui.lineEditGive.setText(password)
+
+
+if __name__ == "__main__":
+
+	app = QtGui.QApplication(sys.argv)
+	myapp = Pass()
+	myapp.show()
+
+	sys.exit(app.exec_())
+
