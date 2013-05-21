334c9dee9b8f01b45b1367909f8b47e0519e1e47
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2013-05-21 17:32:55 +0300

    add more tests

---
+++ tests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+
+import sys
+sys.path.append("./tests")
+
+import test_keeper 
---
+++ tests/test_keeper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import os,sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)),'maxi'))
+
+from keeper import Keeper
+
+k = Keeper()
+print str(k.db)
+k.load_kdb("./tests/test.kdb", "123")
+print str(k.db)
+print k.urls
