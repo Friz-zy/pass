4c76c06c4e7b7d6fa02f7210266e91305e975cef
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2013-07-24 18:08:04 +0300

    refactoring generator

--- maxi/generator.py
+++ maxi/generator.py
@@ -15,26 +15,33 @@ class Generator():
       
       
 	def set_salt(self, file):
-		# check the unique salt
+		if not self.check_salt(file):
+			self.generate_salt(file)
+		with open(file, "r") as f:
+			self.salt = f.read()
+
+	def check_salt(self, file):
+		"default salt have ## in last string by default"
 		salt = "##"
 		with open(file, "r") as f:
 			salt = f.readlines()
-		# if salt isn't unique: add some unique string to it
-		if "#" in salt[-1][1]:
-			try:
-				os.popen("(dmesg;env;head -c16 /dev/random)|sha512sum >> " + file)
-				with open(file, "r") as f:
-					salt = f.readlines()
-				salt[-1]="#" + salt[-1]
-				with open(file, "w") as f:
-					f.writelines(salt)
-				os.popen("chmod 400 " + file) # only your user can read this file
-			except IOError, OSError:
-				# it is works in all OS, but clock() is most efficient in Windows
-				with open(file, "a") as f:
-					f.writeline("#" + hashlib.sha512(clock()).hexdigest())
-		with open(file, "r") as f:
-			self.salt = f.read()
+		if "#" in salt[-1][1] or "#" not in salt[-1][0]:
+			return False
+		return True
+
+	def generate_salt(self, file):
+		try:
+			os.popen("(dmesg;env;head -c16 /dev/random)|sha512sum >> " + file)
+			with open(file, "r") as f:
+				salt = f.readlines()
+			salt[-1]="#" + salt[-1]
+			with open(file, "w") as f:
+				f.writelines(salt)
+			os.popen("chmod 400 " + file) # only your user can read this file
+		except IOError, OSError:
+			# it is works in all OS, but clock() is most efficient in Windows
+			with open(file, "a") as f:
+				f.writeline("#" + hashlib.sha512(clock()).hexdigest())
 
 	def generate_simple(self, nikname = "", url = "", password = "", lenght = 32):
 		lenght = int(lenght)
