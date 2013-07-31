f355feaa1cd6e52c708d6d94dff83642d040e0b1
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2013-07-31 18:10:56 +0300

    add new functional with json, update kdb section in sync function

--- maxi/keeper.py
+++ maxi/keeper.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python2
 # coding=utf-8 
-"""
-self.entries = []
 
-
-add_entry(self,path,title,username,password,url="",notes="",imageid=1,append=True):
-new_entry = infoblock.EntryInfo()
-            new_entry.uuid = self.gen_uuid()
-            new_entry.groupid = group.groupid
-            new_entry.imageid = imageid
-            new_entry.title = title
-            new_entry.url = url
-            new_entry.username = username
-            new_entry.password = password
-            new_entry.notes = notes
-            
-use ony Internet group
-"""
+import os
 try:
 	from keepass import kpdb
-except: pass
+except:
+	import json
 
 
 class Keeper():
@@ -28,23 +14,26 @@ class Keeper():
 			self.db = kpdb.Database()
 			self.isKdb = True
 		except:
-			self.db = None
 			self.isKdb = False
 		self.urls = {}
 		
 	def load(self, file, password = ""):
-		if self.isKdb:
-			self.db = kpdb.Database(str(file), str(password))
-			for entry in self.db.entries:
-				self.urls[entry.url] = {entry.username : [entry.password, entry.notes]}
-		else:
-			pass
+		if os.path.exists(file):
+			if self.isKdb:
+				self.db = kpdb.Database(str(file), str(password))
+				for entry in self.db.entries:
+					self.urls[entry.url] = {entry.username:
+						    [entry.password, entry.notes]}
+			else:
+				with open(file, "r") as f:
+					self.urls = json.loads(f.read())
 	
 	def sync_entries(self):
 		if self.isKdb:
 			urls = {}
 			for entry in self.db.entries:
-				urls[entry.url] = {entry.username : [entry.password, entry.notes]}
+				urls[entry.url] = {entry.username:
+					    [entry.password, entry.notes]}
 			# delete all entries than not in urls:pass
 			for url in urls.keys():
 				for name in urls[url].keys():
@@ -58,9 +47,39 @@ class Keeper():
 				for name in self.urls[url].keys():
 					try:
 						if name not in urls[url].keys(): 
-							self.db.add_entry("Internet"," on ".join((name, url)),name,self.urls[url][name][0],url,self.urls[url][name][1],imageid=1,append=True)
+							self.db.add_entry(
+							  "Internet",
+							  " on ".join((name, url)),
+							  name,
+							  self.urls[url][name][0],
+							  url,
+							  self.urls[url][name][1],
+							  imageid=1,
+							  append=True)
+						else:
+							self.db.update_entry(
+							  " : ".join((url, name)),
+							  name,
+							  url,
+							  new_title=None,
+							  new_username=None,
+							  new_password=self.urls[url][name][0],
+							  new_url=None,
+							  new_notes=self.urls[url][name][1])
 					except:
-						self.db.add_entry("Internet"," on ".join((name, url)),name,self.urls[url][name][0],url,self.urls[url][name][1],imageid=1,append=True)
+						self.db.add_entry(
+						  "Internet",
+						  " on ".join((name, url)),
+						  name,
+						  self.urls[url][name][0],
+						  url,
+						  self.urls[url][name][1],
+						  imageid=1,
+						  append=True)
+		else:
+			for url in self.urls.keys():
+				for name in self.urls[url].keys():
+					self.urls[url][name][0] = "password"
 	
 	def save(self, file, password = ""):
 		if file:
@@ -69,6 +88,7 @@ class Keeper():
 				self.sync_entries()
 				self.db.write(str(file), str(password))
 			else:
-				#with open("urls", "w") as f:
-				#	f.write('\n'.join((self.urls)))
-				pass
+				if file[-4:] != ".json": file += ".json"
+				self.sync_entries()
+				with open(file, "w") as f:
+					f.write(json.dumps(self.urls))
