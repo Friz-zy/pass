d5bae108da0a745f502cc5c9faf39f7e37fd14ba
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2013-12-19 16:00:27 +0300

    revert pass.py

--- maxi/pass.py
+++ maxi/pass.py
@@ -11,24 +11,24 @@
 
 # use this code as salt and check the unique
 with open('pass.py') as f:
-	salt = f.readlines()
+    salt = f.readlines()
 # if salt isn't unique: add some unique string to it
 if "#" in salt[-1][1]:
-	import os
-	try:
-		# only linux way
-		os.popen("(dmesg;env;head -c16 /dev/random)|sha512sum >> ./pass.py")
-		with open('pass.py', "r") as f:
-			salt = f.readlines()
-		salt[-1]="#" + salt[-1]
-		with open('pass.py', "w") as f:
-			f.writelines(salt)
-		os.popen("chmod 400 ./pass.py") # only your user can read this file
-	except IOError, OSError:
-		# it is works in all OS, but clock() is most efficient in Windows
-		from time import clock
-		with open('pass.py', "a") as f:
-			f.writeline("#" + hashlib.sha512(clock()).hexdigest())
+    import os
+    try:
+        # only linux way
+        os.popen("(dmesg;env;head -c16 /dev/random)|sha512sum >> ./pass.py")
+        with open('pass.py', "r") as f:
+            salt = f.readlines()
+        salt[-1]="#" + salt[-1]
+        with open('pass.py', "w") as f:
+            f.writelines(salt)
+        os.popen("chmod 400 ./pass.py") # only your user can read this file
+    except IOError, OSError:
+        # it is works in all OS, but clock() is most efficient in Windows
+        from time import clock
+        with open('pass.py', "a") as f:
+            f.writeline("#" + hashlib.sha512(clock()).hexdigest())
 
 import hashlib, getpass
 
@@ -36,16 +36,15 @@ from itertools import cycle, izip
 
 # XOR function, thk The Internet
 def xor(ss, key):
-	key = cycle(key)
-	return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(ss, key))
+    key = cycle(key)
+    return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(ss, key))
 
 # use this code as salt
 with open('pass.py') as f:
-	salt = f.read()
+    salt = f.read()
 
 nick = raw_input("nick:") + " : " + raw_input("site:")
 firstXor = xor(salt, nick)
 firstSha = hashlib.sha512(firstXor).hexdigest()
 print hashlib.sha512(xor(firstSha, getpass.getpass())).hexdigest()[:32]
 ##
-#08f0e8e58d1b9960a881fcc727c65c3a9821dbcb84563a4f0b6e0bd7b8131e4c6f3f1dc0093b8e4285e46948dbc51700176b4c7fc88976dc068014d6965a3bef  -
