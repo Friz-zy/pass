38e7fbbff516064d6836372b35ab0098727ec6f5
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-01-08 04:01:25 +0300

    some update of setup.py for python3

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 import shutil, errno, os
-target = "maxi" # import mini or maxi
 
-root, dirs, files = os.walk('.').next() 
-
-dotDirs=[]
-for D in dirs:
-    if D[0] != ".":
-        shutil.move(D, "." + D)
-        dotDirs.append(D)
-
-shutil.copytree("." + target, "paSs")
-
-if not os.path.exists("pass.egg-info"):
-    os.makedirs("pass.egg-info")
-for F in files:
-    if ".md" in F:
-        shutil.copy(F, "pass.egg-info")
-
-
-import paSs as module
+import Pass as module
 setup(
-    name='pass',
+    name='Pass',
     version = module.__version__,
     author = module.__author__,
     author_email = module.__email__,
@@ -31,35 +13,24 @@ setup(
     keywords = module.__keywords__,
     url = module.__url__,   # project home page, if any
     install_requires = ['PySide', 'pycrypto', 'six'],
+    packages=['Pass'],
     package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst', '*.md'],
-        # And include any *.msg files found in the 'hello' package, too:
-        "paSs" : ['images/*', 'icons/*', 'keepass/*', '*.*'],
+        '.': ['*.txt', '*.rst', '*.md'],
+        "Pass" : ['images/*', 'icons/*', 'keepass/*', '*.*'],
     },
-    packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     include_package_data=True,
-    test_suite='tests',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
     entry_points={
         'console_scripts':
             ['pass = paSs.window:main',],
         'gui_scripts':
             ['pass = paSs.window:main',],
-#        'setuptools.installation':
-#            ['eggsecutable = ' + md + '.window',],
         }
 )
-
-shutil.rmtree("paSs")
-shutil.rmtree("build")
-shutil.rmtree("pass.egg-info")
-
-for D in dotDirs:
-    shutil.move("." + D, D)
-
-root, dirs, files = os.walk('.').next()
-for F in files:
-    if ".pyc" in F:
-        os.remove(F)
-
